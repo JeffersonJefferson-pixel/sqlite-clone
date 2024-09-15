a516fa7cc868824e28fd7c09cec86af4a27c4b27
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-09-15 12:14:43 +0700

    fix error

--- vm.c
+++ vm.c
@@ -21,6 +21,31 @@ static void deserialize_row(void *source, Row* destination) {
   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+// create a cursor at the beginning of the table.
+static Cursor* table_start(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+// create a cursor at the end of the table.
+static Cursor* table_end(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+}
+
+static void cursor_advance(Cursor* cursor) {
+  cursor->row_num += 1;
+  if (cursor->row_num >= cursor->table->num_rows) {
+    cursor->end_of_table = true;
+  }
+}
+
 // return a pointer to the position in page described by the cursor.
 static void* cursor_value(Cursor* cursor) {
   uint32_t row_num = cursor->row_num; 
@@ -58,7 +83,7 @@ static ExecuteResult execute_select(Statement* statement, Table* table) {
 
   Row row;
   while (!(cursor->end_of_table)) {
-    deserialize_row(row_slot(table), &row);
+    deserialize_row(cursor_value(cursor), &row);
     print_row(&row);
     cursor_advance(cursor);
   }
@@ -130,30 +155,5 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
       return execute_insert(statement, table);
     case (STATEMENT_SELECT):
       return execute_select(statement, table);
-  }
-}
-
-// create a cursor at the beginning of the table.
-Cursor* table_start(Table* table) {
-    Cursor* cursor = malloc(sizeof(Cursor));
-    cursor->table = table;
-    cursor->row_num = 0;
-    cursor->end_of_table = (table->num_rows == 0);
-
-    return cursor;
-}
-
-// create a cursor at the end of the table.
-Cursor* table_end(Table* table) {
-    Cursor* cursor = malloc(sizeof(Cursor));
-    cursor->table = table;
-    cursor->row_num = table->num_rows;
-    cursor->end_of_table = true;
-}
-
-void cursor_advance(Cursor* cursor) {
-  cursor->row_num += 1;
-  if (cursor->row_num >= cursor->table->num_rows) {
-    cursor->end_of_table = true;
   }
 }
